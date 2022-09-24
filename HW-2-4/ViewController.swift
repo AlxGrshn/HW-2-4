43e8be881d2a44c1ec449986fe936c2ecf545e97
Frysed <frysed@gmail.com>
2022-09-25 00:30:32 +0300

    add base logic

--- HW-2-4/ViewController.swift
+++ HW-2-4/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var bluerLabel: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabel.text = redSlider.value.formatted()
+    }
+    
+    @IBAction func blueSliderAction() {
+        bluerLabel.text = blueSlider.value.formatted()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = greenSlider.value.formatted()
+    }
+    
+    
 }
 
